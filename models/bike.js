c913115052fddec26d94a7af896bdb12461bd923
DmytroVoitovych <begunec23@gmail.com>
2023-05-07 18:21:02 +0300

    created a model for data in the database

--- models/bike.js
+++ models/bike.js
@@ -1,7 +1,48 @@
 const { Schema, model } = require("mongoose");
 
 const bikesSchema = Schema(
-  {},
+  {
+    Departure: {
+      type: Date,
+      required: [true, "Type is required"],
+      alias: "dp",
+    },
+    Return: {
+      type: Date,
+      required: [true, "Type is required"],
+      alias: "ret",
+    },
+    DepartureStationId: {
+      type: String,
+      required: [true, "Type is required"],
+      alias: "dpStId",
+    },
+    DepartureStationName: {
+      type: String,
+      required: [true, "Type is required"],
+      alias: "dpStNa",
+    },
+    ReturnStationId: {
+      type: String,
+      required: [true, "Type is required"],
+      alias: "retStId",
+    },
+    ReturnStationName: {
+      type: String,
+      required: [true, "Type is required"],
+      alias: "retStNa",
+    },
+    CoveredDistance: {
+      type: String,
+      required: [true, "Type is required"],
+      alias: "covDis",
+    },
+    Duration: {
+      type: String,
+      required: [true, "Type is required"],
+      alias: "dur",
+    },
+  },
   { versionKey: false, timestamps: false }
 );
 
